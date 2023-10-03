d5300085b7b27dc25ea9f944beb3f0fa0beed830
Zulqarnain Sarwar <zulqarnainsarwar7@gmail.com>
2023-10-03 17:57:47 +0500

    mobile fixing

--- src/components/common/Heading.js
+++ src/components/common/Heading.js
@@ -6,14 +6,14 @@ const Heading = ({ text, paragraph, top, left, secn, className }) => {
     left: `${left}%`,
   };
   return (
-    <div className={`absolute ${className}`} style={style}>
-      <span className="text-lg font-montserat bg-[#5E41A9] text-white w-[102px] text-center absolute top-[-10vh] lg:top-[-25vh] lg:left-[0vw]">
+    <div className={`absolute ${className} text-div`} style={style}>
+      <span className="text-lg font-montserat bg-[#5E41A9] text-white w-[102px] text-center absolute top-[-10vh] lg:top-[-25vh] lg:left-[0vw] text-span">
         {secn}
       </span>
-      <h1 className="lg:text-2xl  uppercase text-white font-sharpin  min-w-full text-md  ">
+      <h1 className="lg:text-2xl  uppercase text-white font-sharpin  min-w-full text-md text-heading ">
         {text}
       </h1>
-      <p className="lg:text-lg font-montserat py-4 text-[#B8B8B8] text-sm ">
+      <p className="lg:text-lg font-montserat py-4 text-[#B8B8B8] text-sm text-paragraph">
         {paragraph}
       </p>
     </div>
